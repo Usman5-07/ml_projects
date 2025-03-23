51d84427af192d5201a6a4f2ca4b8aedc0817d85
Usman5-07 <muhammad.u.dev@gmail.com>
2025-03-23 21:57:49 +0500

    Logger and exception

---
+++ src/exceptions.py
@@ -0,0 +1,36 @@
+import sys
+import logging
+import logger
+
+
+def error_message_details(error, error_details: sys):
+    _, _, exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = "Error occurred in python script [{0}] line number [{1}] Error Message: [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_details=error_details)
+
+    def __str__(self):
+        return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Cannot divide by 0")
+        raise CustomException(e, sys)
+    logging.basicConfig(
+
+        filename=logger.LOG_FILE_PATH,
+        format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
